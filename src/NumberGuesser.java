c01a0ac6c9710e855b21a14f7fe84e488e079160
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2017-03-21 15:25:32 +0100

    feat(guess): program guesses between 0 and 4
    The program shows doubles instead of integers
    It uses an extra step on the higher boundary

--- src/NumberGuesser.java
+++ src/NumberGuesser.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+import java.lang.Math;
+
 public class NumberGuesser {
   public static void main(String[] args) {
-    System.out.println("Please think a number between 0 and 50!");
+    double max = 4.;
+    double min = 0.;
+    System.out.println("Please think a number between 0 and 4!");
+    double currentHalf = max / 2;
+
+    while (min < max) {
+      System.out.println("Is your number under " + currentHalf + "? [y/n]");
+      Scanner scanner = new Scanner(System.in);
+      String answer = scanner.nextLine();
+      if (answer.equals("y")) {
+        max = currentHalf - 1;
+        currentHalf = Math.round((currentHalf + min) / 2);
+      } else {
+        min = currentHalf;
+        currentHalf = Math.round((currentHalf + max) / 2);
+      }
+    }
+    System.out.println("Your number is: " + min);
   }
 }
 
